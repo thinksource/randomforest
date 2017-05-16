eb4e36e2c7cf21c65bb2fb036d088b5d7dfc1fc7
thinksource <foretribe@gmail.com>
2017-05-17 04:24:31 +1000

    init re

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, current_app
+from flask import request
+from flask import render_template
+from myforest import *
+import numpy as np
+
+import os
+basedir = os.path.join(os.path.dirname(os.path.abspath(".")),"randomforest")
+
+
+app = Flask(__name__, template_folder=os.path.join(basedir, 'templates'))
+with app.app_context():
+    # within this block, current_app points to app.
+    print(current_app.name+" running.")
+
+# my_loader = jinja2.ChoiceLoader([
+#         app.jinja_loader,
+#         jinja2.FileSystemLoader(['/flaskapp/userdata',
+#                                  '/flaskapp/templates']),
+#     ])
+
+@app.route('/',  methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        var1 = request.form['VAR1']
+        var2 = request.form['VAR2']
+        var3 = request.form['VAR3']
+        var4 = datatype(request.form['VAR4'])
+        var5 = datatype(request.form['VAR5'])
+        var6 = datatype(request.form['VAR6'])
+        var7 = datatype(request.form['VAR7'])
+        var8 = datatype(request.form['VAR8'])
+        var9 = datatype(request.form['VAR9'])
+        var10 = datatype(request.form['VAR10'])
+        var=np.asarray([var4,var5,var6,var7,var8,var9,var10,var1,var2,var3]).reshape(1,-1)
+        print(var)
+        model=app.config["model"]
+        result=model.predict(var)
+        prodict=model.predict_proba(var)
+        return render_template("result.html", result=result, prodict="{0:.1f}%".format(prodict[0][0]*100))
+
+if __name__ == '__main__':
+    df=getdataframe()
+    #data,target=np.split(df.values, (10,), axis=1)
+    a, target = np.split(df.values, (10,), axis=1)
+    #x_train, x_test, y_train, y_test = train_test_split(data, target.reshape(-1), test_size=0.3, random_state=1)
+    model=RandomForestClassifier(n_estimators=200, max_features=4,criterion='gini')
+    rf_clf=model.fit(a, target.reshape(-1))
+    procentage="{0:.1f}%".format(rf_clf.score(a, target.reshape(-1))*100)
+    print("The Random Forest prodict accuracy procentage:"+procentage)
+    app.config["model"]=rf_clf
+    app.config["tt"] = "erer"
+    #re=predictrate(x_test, y_test.reshape(-1), rf_clf)
+    app.run()
--- myforest.py
+++ myforest.py
@@ -5,16 +5,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy.lib.recfunctions as rfn
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sqlalchemy import create_engine
 
+path1 = 'table1.csv'
+path2 = 'table2.csv'
+dbconnector='mysql+mysqlconnector://root:root@localhost/randomforst'
+iris_feature = u'VAR4', u'VAR5', u'VAR6', u'VAR7'
 def datatype(s):
-    #﻿it={'ID':0,'VAR1':1,'VAR2':2,'VAR3':3,'VAR4':4,'VAR5':5,'VAR6':6,'VAR7':7,'VAR8':8,'VAR9':9,'VAR10':9,'OUTCOME':10}
-    it={'N':0,'L':1,'H':2}
-    return it[str(s.strip(), 'utf-8')]
+    it={'H':0,'L':1,'N':2}
+    return it[s.strip()]
+
+
+def getdataframe():
+    engine = create_engine(dbconnector)
+    df1=pd.read_csv(path1, index_col='ID')
+    df2=pd.read_csv(path2, index_col='ID',converters={1: datatype, 2:datatype,3:datatype,4:datatype,5:datatype,6:datatype,7:datatype})
+    df=pd.merge(df2,df1, right_index=True,left_index=True)
+    df.to_sql("sample",engine,if_exists='replace')
+    return df
+
+def trainmodel(data, target,model):
+    rf_clf=model.fit(data, target)
+    return rf_clf
+
+def predictrate(data, target, model):
+    r_hat=model.predict(data)
+    re = (r_hat == target)
+    return np.mean(re)
+
+
+def work():
+    df=getdataframe()
+    data,target=np.split(df.values, (10,), axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(data, target.reshape(-1), test_size=0.3, random_state=1)
+    model=RandomForestClassifier(n_estimators=200, max_features=4,criterion='gini')
+    rf_clf=model.fit(x_train, y_train.reshape(-1))
+    re=predictrate(x_test, y_test.reshape(-1), rf_clf)
+    return re
 
 if __name__ == "__main__":
-    path1 = 'table1.csv'
-    path2 = 'table2.csv'
-    data1 = np.loadtxt(path1, dtype=[('id', int),('VAR1', int),('VAR2', int),('VAR3', float),('OUTCOME', int)], delimiter=',', skiprows=1)
-    data2 = np.loadtxt(path2, dtype=[('id', int),('VAR4', int),('VAR5', int),('VAR6', int),('VAR7', int),('VAR8', int),('VAR9', int)],delimiter=',', skiprows=1,converters={1: datatype, 2:datatype,3:datatype,4:datatype,5:datatype,6:datatype})
-    data =rfn.join_by('id', data1,data2, jointype='inner' usemask=False)
+    df=getdataframe()
+    data,target=np.split(df.values, (10,), axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(data, target.reshape(-1), test_size=0.3, random_state=1)
+    model=RandomForestClassifier(n_estimators=200, max_features=4,criterion='gini')
+    rf_clf=model.fit(x_train, y_train.reshape(-1))
+    re=predictrate(x_test, y_test.reshape(-1), rf_clf)
